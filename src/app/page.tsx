ab6a4a9d7fee48f75aae8936b9aa5b9b44b9cff6
D4rzDev <darelhonrejas86@gmail.com>
2024-06-04 13:56:34 +0800

    remove section2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
         <Navbar/>
         <main>
           <Hero/>
-          <Section2/>
+          {/* <Section2/> */}
           <Section3/>
           <Section4/>
           <Section5/>
