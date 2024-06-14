99c33b6dbc5cef1136e54ddae58e6284c6ec8788
D4rzDev <darelhonrejas86@gmail.com>
2024-06-14 17:28:00 +0800

    download link fix

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -18,10 +18,10 @@ export default function page() {
             <p className=' text-sm text-orange-100 text-center'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 
             <div className=' flex justify-center items-center gap-2 mt-6 clear-start bg-orange-300 p-2 rounded-lg'>
-              <a href="https%3A%2F%2Fwww.apple.com%2Fph%2Fapp-store%2F&h=AT06sn-oeG_i_XG4pmAtm30gX6SchnmLBU9ULTWcAulaYYDvWpCcmizpjT9ghXN2qg5qKwTSZOelFgIrH-gRvhBZHpbXxkzFwWIydrZAiE1g3npMv4Dq4rk8TncmuLpZln0m4ITmGtLaHfLYHo8ZFQ">
+              <a href="https://www.apple.com/ph/app-store/">
                 <img src="/astore.png" alt="" width={200} className=' 2xl:w-[200px] w-[120px] drop-shadow-md shadow-white' />
               </a>
-              <a href="https%3A%2F%2Fplay.google.com%2Fstore%2Fgames%3Fhl%3Den%26fbclid%3DIwZXh0bgNhZW0CMTAAAR1ULLl-xf7gRNk3sW6OQyAjRKtWjZ9cIAtc1c8xnhpzFXd1JWxLTCnN9cs_aem_ZmFrZWR1bW15MTZieXRlcw&h=AT06sn-oeG_i_XG4pmAtm30gX6SchnmLBU9ULTWcAulaYYDvWpCcmizpjT9ghXN2qg5qKwTSZOelFgIrH-gRvhBZHpbXxkzFwWIydrZAiE1g3npMv4Dq4rk8TncmuLpZln0m4ITmGtLaHfLYHo8ZFQ">
+              <a href="https://play.google.com/store/games?hl=en&fbclid=IwZXh0bgNhZW0CMTAAAR3ly3HMKLmI37Cmp0M9GC_5BWBtQUhpmYCU7yAnNCivWihGVzxuzcRe1Ik_aem_AT5s9sJFhCA_hWjYwNGU503hjbYLrkq8ihii4GCibOKImBOF_B4rG42Swv8x6xcrcQ7rg5GffIbUYPGF43_j0t15">
               <img src="/gp.png" alt="" width={200} className=' 2xl:w-[200px] w-[120px]' />
 
               </a>
