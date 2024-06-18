1082aa5abd3a2af0b4e5a5c5e832420bd2fd7bc2
D4rzDev <darelhonrejas86@gmail.com>
2024-06-18 17:48:36 +0800

    navbar

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -55,10 +55,10 @@ export default function Hero() {
 
         </div>
 
-        <div className='relative max-w-[1920px] w-[90%] lg:w-[78%] h-full flex items-end justify-end py-20'>
+        <div className='relative z-0 max-w-[1920px] w-[90%] lg:w-[78%] h-[90%] flex items-end justify-end py-20'>
           <img src="/v2/header/assets/Character.png" alt="" width={520} className=' lg:w-[280px] xl:w-[290px] 2xl:w-[380px] absolute right-0 bottom-0 z-20 lg:block hidden' />
 
-          <div className=' relative h-[90%] grid grid-cols-1 lg:grid-cols-2 rounded-lg'
+          <div className=' relative h-full grid grid-cols-1 lg:grid-cols-2 rounded-lg'
         style={{backgroundImage: "url('/v2/header/assets/BG A.png')", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat:"no-repeat"}}
         >
           <div className=' relative z-20 flex flex-col justify-center md:p-14 p-8'>
